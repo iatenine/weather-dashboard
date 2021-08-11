79fb951b8f2876534e6c7369d2f7af82e5670821
Jack Linhart <iatenine@gmail.com>
2021-08-10 21:58:52 -0600

    jquery value grab

--- assets/app.js
+++ assets/app.js
@@ -1,1 +1,13 @@
 const apiKey = "c1ecab3291362f3fe61c8735f3bd9de6";
+
+console.log("Hello world");
+
+$(document).ready(function () {
+  $("#submit").click(function (e) {
+    e.preventDefault();
+    const city = $("#city").val();
+    console.log("click", city);
+  });
+});
+
+const getWeather = async (city) => {};
