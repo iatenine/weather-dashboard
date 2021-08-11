df23b12f6ff87d0da488342811fdef44850bf2b8
Jack Linhart <iatenine@gmail.com>
2021-08-10 22:23:28 -0600

    fetch weather data

--- assets/app.js
+++ assets/app.js
@@ -1,13 +1,29 @@
 const apiKey = "c1ecab3291362f3fe61c8735f3bd9de6";
 
-console.log("Hello world");
+function handleWeatherRes(response) {
+  console.log("handling response: ", response);
+}
 
+// Respond to button click
 $(document).ready(function () {
   $("#submit").click(function (e) {
     e.preventDefault();
     const city = $("#city").val();
-    console.log("click", city);
+    getWeather(city);
   });
 });
 
-const getWeather = async (city) => {};
+const getWeather = async (city) => {
+  const settings = {
+    url: `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${apiKey}`,
+    method: "GET",
+    timeout: 0,
+    error: function () {
+      console.error("Error, double check the city name entered and try again");
+    },
+  };
+
+  $.ajax(settings).done(function (response) {
+    handleWeatherRes(response);
+  });
+};
