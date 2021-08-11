bcf5ccefd5a75457986f879d5f21184b65536a07
Jack Linhart <iatenine@gmail.com>
2021-08-10 22:32:55 -0600

    fetch current weather data (not just forecast)

--- assets/app.js
+++ assets/app.js
@@ -1,7 +1,12 @@
 const apiKey = "c1ecab3291362f3fe61c8735f3bd9de6";
+const units = "imperial";
 
-function handleWeatherRes(response) {
-  console.log("handling response: ", response);
+function handleForecastData(response) {
+  console.log("5-Day Forecast: ", response);
+}
+
+function handleWeatherData(response) {
+  console.log("Current Weather: ", response);
 }
 
 // Respond to button click
@@ -9,11 +14,12 @@ $(document).ready(function () {
   $("#submit").click(function (e) {
     e.preventDefault();
     const city = $("#city").val();
-    getWeather(city);
+    getForecast(city);
+    getCurrentWeather(city);
   });
 });
 
-const getWeather = async (city) => {
+const getForecast = async (city) => {
   const settings = {
     url: `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${apiKey}`,
     method: "GET",
@@ -24,6 +30,21 @@ const getWeather = async (city) => {
   };
 
   $.ajax(settings).done(function (response) {
-    handleWeatherRes(response);
+    handleForecastData(response);
+  });
+};
+
+const getCurrentWeather = async (city) => {
+  var settings = {
+    url: `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&appId=${apiKey}`,
+    method: "GET",
+    timeout: 0,
+    error: function () {
+      console.error("Error, double check the city name entered and try again");
+    },
+  };
+
+  $.ajax(settings).done(function (response) {
+    handleWeatherData(response);
   });
 };
